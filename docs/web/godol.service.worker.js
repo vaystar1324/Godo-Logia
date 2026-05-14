ddc3754e3307967f26e7d3059a507aa6bb785986
vaystar1324 <vaystar994@gmail.com>
2026-05-14 16:22:31 +0300

    9

--- docs/web/godol.service.worker.js
+++ docs/web/godol.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1778130564|1013688318';
+const CACHE_VERSION = '1778764902|8017751259';
 /** @type {string} */
 const CACHE_PREFIX = 'GodoLogia-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
